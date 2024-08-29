8e047b35199cce43f58de6d9ca05e2d7cbfff068
Blake Sergesketter <blakesergesketter@gmail.com>
2024-08-29 13:47:40 -0500

    Creation of the TemplateModel, id and body columns, and a little bit of sudocoding.

--- api.py
+++ api.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# configure our database
 db = SQLAlchemy(app)
+# Flask utilizes sqlite3 with the use of SQLAlchemy 
 
-class UserModel(db.Model):
-    id = db.Colum
+class TemplateModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(100), nullable=True)
 
-@app.route("/")
-def check():
-    return '<h1>Flask is working</h1>'
+template_args = reqparse.RequestParser()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
