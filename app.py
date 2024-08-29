6e67bdbe9bfc4b5e2a531ec8bb1f0015e02541f7
Blake Sergesketter <blakesergesketter@gmail.com>
2024-08-28 20:32:02 -0500

    some minor additions to app.py solely for practice/testing purpose.

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+@app.route("/")
+def check():
+    return 'Flask is working'
+
+if __name__ == "__main__":
+    app.run()
